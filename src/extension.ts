7006fdd7aaacb5ad8afaf32d479355961419d026
Paul Bin <paul.bin0117@gmail.com>
2026-01-30 23:59:45 +0900

    added buttons for commit and commit warning

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,27 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let statusBarItem: vscode.StatusBarItem;
+
 export function activate(context: vscode.ExtensionContext) {
+	console.log('SubText is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "subtext" is now active!');
+	// Register command
+	let disposable = vscode.commands.registerCommand('subtext.generateCommit', () => {
+		vscode.window.showInformationMessage('✨ SubText is analyzing your changes...');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('subtext.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from SubText!');
+		// TODO: add logic
 	});
 
+	// Create the Status Bar Item
+	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	statusBarItem.text = "$(telescope) SubText: Ready";
+	statusBarItem.tooltip = "Click to analyze diff";
+	statusBarItem.command = "subtext.generateCommit";
+	statusBarItem.show();
+
+	// Register command and status bar item
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(statusBarItem);
 }
 
 // This method is called when your extension is deactivated
